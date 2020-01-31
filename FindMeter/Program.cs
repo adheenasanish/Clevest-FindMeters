67c83afa0606d8ec339e0cd6e080b3f482d2a292
adhee <adheenaus@gmail.com>
2020-01-31 10:07:35 -0800

    Add project files.

---
+++ FindMeter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindMeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
