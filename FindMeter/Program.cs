984fe9d3e2849d3aea84d2a38da73bcca7f7ff16
adhee <adheenaus@gmail.com>
2020-01-31 10:11:23 -0800

    firstCommit

--- FindMeter/Program.cs
+++ FindMeter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace FindMeter
 {
@@ -6,7 +8,48 @@ namespace FindMeter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char[] charArray = { };
+            int countFloor = 0;
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("C://USers/adhee/source/repos/FindMeter/question01_input.txt"))
+                {
+                    // Read the stream to a string
+                    string line = sr.ReadToEnd();
+                    int l = line.Length;
+                    charArray = new char[line.Length];
+
+                    using (StringReader stringReader = new StringReader(line))
+                    {
+                        // Read characters from the string into the array.
+                        stringReader.Read(charArray, 0, line.Length);
+
+                    }
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (charArray[i] == '(')
+                {
+                    countFloor++;
+                }
+                else
+                {
+                    countFloor--;
+                }
+            }
+
+
+            Console.WriteLine("The instruction will take the worker in to the {0} th floor ", countFloor);
+
+            Console.ReadLine();
         }
     }
 }
